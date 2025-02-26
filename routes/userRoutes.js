ea71703c59b2561d075fc2e48628d2d3059e28ec
Mani Rastegari <mani.rastegari@gmail.com>
2025-02-26 00:14:44 -0500

    EventIdRouteAddedV7

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -116,7 +116,7 @@ router.get('/details/:id', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-// // Get user details with posted arts and events
+// // Get user details with posted arts and events 11
 // router.get('/details/:id', async (req, res) => {
 //   try {
 //     const user = await User.findById(req.params.id)
