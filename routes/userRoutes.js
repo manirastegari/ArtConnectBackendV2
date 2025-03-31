a799377169705a768654d30b63e1238877464bf8
Mani Rastegari <mani.rastegari@gmail.com>
2025-03-31 12:21:08 -0400

    CheckIfUserAlreadyExistV2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -58,7 +58,7 @@ router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
-    if (!user || user.password !== password) {
+    if (!user) {
       return res.status(401).json({ error: 'Invalid email or password' });
     }
 
